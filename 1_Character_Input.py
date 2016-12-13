ee057f7efff78209369e9b57cc28485dc9ffe8af
Laurendv <laurendvu@outlook.com>
2016-12-13 12:56:05 +0000

    Exercise 1

---
+++ 1_Character_Input.py
@@ -0,0 +1,4 @@
+name = raw_input('Give me your name :')
+age = int(input('Give me your age :'))
+age100 = 2016 + (100 - age)
+print('Your name is ' + name + '.' + '\nYou will turn 100 in ' + str(age100) +'.')
